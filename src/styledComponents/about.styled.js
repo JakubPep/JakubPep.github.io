c838075daa621094a9c321fcc839fa599ff88bf8
Jakub Pepliński <peplinjak@gmail.com>
2023-03-28 19:53:19 +0200

    about paragraph size down

--- src/styledComponents/about.styled.js
+++ src/styledComponents/about.styled.js
@@ -6,34 +6,35 @@ export const AboutContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    gap: 46px;
+    gap: 20px;
     margin-bottom: 60px;
 `
 
 export const AboutDescription = styled.div`
     display: flex;
     flex-direction: column;
-    flex-basis: 45%;
+    flex-basis: 40%;
     gap: 20px;
     h3 {
         color: ${COLORS.font_black};
     }
     p {
-        font-size: 22px;
-        line-height: 32px;
+        font-size: 18px;
+        line-height: 24px;
         color: ${COLORS.font_black};
+        margin: 0;
     }
 `
 
 export const AboutPhoto = styled.div`
-    width: 600px;
-    height: 600px;
+    width: 30vw;
+    height: 30vw;
     margin: 10px;
 
     background-image: url(${circlePhoto});
     background-size: cover;
-    background-position: center;
+    background-position: center ;
 
-    border: 10px solid ${COLORS.light_pink};
+    border: 7px solid ${COLORS.light_pink};
     border-radius: 50%;
 `
