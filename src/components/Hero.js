fca23410e57055b6134105b5b948ac3cf86cd1eb
Jakub Pepliński <peplinjak@gmail.com>
2023-04-19 22:57:02 +0200

    Hero logo edited

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { HeroContainer, HeroImage, HeroLogo } from "../styledComponents/hero.styled.js";
+import logoImage from "../images/pracownia-logo.png"
 
 const Hero = (props) => {
+
+  const LogoImage = () => {
+    return(
+      <>
+      <HeroLogo>
+      <img src={logoImage} alt="pracownia-logo"></img>
+      </HeroLogo>
+      </>
+    )
+  }
+
   return (
     <>
     <HeroContainer
     height={props.height}>
       <HeroImage ></HeroImage>
-      <HeroLogo></HeroLogo>
+      <LogoImage></LogoImage>
+      
+      
       
       
       </HeroContainer>
--- src/styledComponents/hero.styled.js
+++ src/styledComponents/hero.styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { COLORS } from "../Vars";
 import bgImage from "../images/heroPhoto.jpg";
-import logoImage from "../images/pracownia-logo.png";
 
 export const HeroContainer = styled.div`
 height: ${(props) => props.height};
@@ -17,14 +16,16 @@ filter: blur(2px);
 `;
 
 export const HeroLogo = styled.div`
-height: 75vh;
-width: 75vh;
-border-radius: 50%;
-
 position: absolute;
-top: 10%;
+top: 5%;
 left: 50%;
+background-color: ${COLORS.light_green_opacity};
+border-radius: 50%;
+img{
+    height: 45vw;
+    width: auto;
+    border-radius: 50%;
 
-background: url(${logoImage}), ${COLORS.light_green_opacity};
-background-size: cover;
+    
+}
 `
